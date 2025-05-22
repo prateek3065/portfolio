7a1f5b88fc4fd31e82cdd7495a2d0a83e3929a89
PRATEEK SHARMA <prateeksharma@PRATEEKs-Laptop.local>
2025-05-22 20:53:21 +0200

    added meta data

--- src/app/[locale]/home/(page)/hero-section/about-me-card/card-body/my-profile-photo/my-profile-photo.tsx
+++ src/app/[locale]/home/(page)/hero-section/about-me-card/card-body/my-profile-photo/my-profile-photo.tsx
@@ -3,7 +3,7 @@ const MyProfilePhoto: React.FC = () => {
   return (
     <div className="w-48 aspect-square rounded-full bg-[#1717176e] overflow-hidden border-2 border-white">
       <Image
-        src="/assets/my-photo.jpeg" // no need for `public/`
+        src="/my-photo.jpeg" // no need for `public/`
         alt="bg-img"
         width={400}
         height={0} // use actual height or use `fill`
