493b8a0ab240e77812dc36c3df4ba10cd9cd67c4
PRATEEK SHARMA <prateeksharma@PRATEEKs-Laptop.local>
2025-05-22 20:29:34 +0200

    added portfolio link

--- src/app/[locale]/myCv/pdf/info-segment/footer/footer.tsx
+++ src/app/[locale]/myCv/pdf/info-segment/footer/footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { StyleSheet, View, Svg, Path, Link } from "@react-pdf/renderer";
+import { StyleSheet, View, Svg, Path, Link, Text } from "@react-pdf/renderer";
 
 const styles = StyleSheet.create({
   container: {
@@ -18,6 +18,16 @@ const styles = StyleSheet.create({
   linkedInContainer: {
     display: "flex",
     paddingLeft: 1,
+  },
+  text: {
+    fontSize: 10,
+    color: "white",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    columnGap: 2,
+    maxWidth: 150,
+    paddingLeft: 5,
   },
 });
 const Footer: React.FC = () => {
@@ -43,6 +53,9 @@ const Footer: React.FC = () => {
         <Svg viewBox="0 0 24 24" fill="white" width="17" height="17">
           <Path d="M0 0v24h24V0zm9.95 8.002h1.805c.061 0 .111.05.111.111v7.767c0 .061-.05.111-.11.111H9.95a.111.111 0 0 1-.111-.11v-2.87H7.894v2.87c0 .06-.05.11-.11.11H5.976a.11.11 0 0 1-.11-.11V8.112c0-.06.05-.11.11-.11h1.806c.061 0 .11.05.11.11v2.869H9.84v-2.87c0-.06.05-.11.11-.11zm2.999 0h5.778c.061 0 .111.05.111.11v7.767a.11.11 0 0 1-.11.112h-5.78a.11.11 0 0 1-.11-.11v-7.77c0-.06.05-.11.11-.11z" />
         </Svg>
+      </Link>
+      <Link href="https://portfolio-pi-rust-90.vercel.app/home">
+        <Text style={styles.text}>My Portfolio</Text>
       </Link>
     </View>
   );
